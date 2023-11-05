5d9b889bbe01fba4e988d15a5b0fc537ff72a81b
Ugnius Saulevičius <ugnius.saulevicius@gmail.com>
2023-11-05 15:07:57 +0200

    add: initial project setup

---
+++ nvsc-lpl-app/src/App.tsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './Router'
+
+export const App = () => {
+
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  )
+}
