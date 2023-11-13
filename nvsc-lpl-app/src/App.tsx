80aff3e992dbfc8ddee469079056a00b0cf0a086
Ugnius Saulevičius <ugnius.saulevicius@gmail.com>
2023-11-13 18:09:09 +0200

    fix: change to hash router for url routing

--- nvsc-lpl-app/src/App.tsx
+++ nvsc-lpl-app/src/App.tsx
@@ -1,13 +1,27 @@
-
-import './App.css'
-import { RouterProvider } from 'react-router-dom'
-import { router } from './Router'
+import './App.css';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import { Home } from "./pages/Home";
+import { ImportantInfo } from "./pages/ImportantInfo";
+import { PreventionInfo } from "./pages/PreventionInfo";
+import { LegionellosisInfo } from "./pages/LegionellosisInfo";
+import { RightsAndObligationsInfo } from "./pages/RightsAndObligationsInfo";
+import { ContactsInfo } from "./pages/ContactsInfo";
+import { WaterTestingInfo } from "./pages/WaterTestingInfo";
+import { FAQ } from "./pages/FAQ";
 
 export const App = () => {
-
   return (
-    <>
-        <RouterProvider router={router} />
-    </>
-  )
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/svarbu-zinoti" element={<ImportantInfo />} />
+        <Route path="/prevencija" element={<PreventionInfo />} />
+        <Route path="/legionelioze" element={<LegionellosisInfo />} />
+        <Route path="/teises-atsakomybes" element={<RightsAndObligationsInfo />} />
+        <Route path="/kontaktai" element={<ContactsInfo />} />
+        <Route path="/geriamojo-vandens-tyrimu-programos" element={<WaterTestingInfo />} />
+        <Route path="/duk" element={<FAQ />} />
+      </Routes>
+    </HashRouter>
+  );
 }
