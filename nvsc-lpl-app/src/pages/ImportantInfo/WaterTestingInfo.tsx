6d1a44317c8b56a68af98901dedc4d0ad791b1ab
Nenumarokas <marius.pazera@gmail.com>
2023-12-11 12:05:59 +0200

    updated water testing info

--- nvsc-lpl-app/src/pages/ImportantInfo/WaterTestingInfo.tsx
+++ nvsc-lpl-app/src/pages/ImportantInfo/WaterTestingInfo.tsx
@@ -10,9 +10,26 @@ export const WaterTestingInfo: React.FC = () => {
         <h1 className="importantHeader">Geriamojo vandens tyrimų programos</h1>
         <hr/>
         
+        <p className='importantMainText'>
+          Geriamojo vandens tiekėjai turi rengti ir vykdyti geriamojo
+          vandens stebėsenos programas. Šios stebėsenos tikslas yra: 
+          <br/><br/>
+            •	tikrinti, ar visoje geriamojo vandens tiekimo grandinėje
+            veiksmingai taikomos rizikos žmonių sveikatai kontrolės priemonės
+            ir ar atitikties vietoje geriamasis vanduo yra saugus ir kokybiškas;
+          <br/>
+            •	nustatyti tinkamiausius rizikos žmonių sveikatai mažinimo būdus.
+        </p>
+
         <a className="importantMainText" href = "https://e-seimas.lrs.lt/portal/legalAct/lt/TAD/TAIS.216309/asr">
-          Vykdomos pagal Lietuvos higienos normos HN 24:2023 Geriamojo vandens saugos ir kokybės reikalavimus
+          •	surinkti informaciją apie geriamojo vandens saugą ir kokybę,
+            siekiant įrodyti, jog laikomasi Higienos normoje nustatytų reikalavimų;
         </a>
+            
+        <p className="importantMainText">
+            
+        </p>
+        
       </div>
     </>
   );
