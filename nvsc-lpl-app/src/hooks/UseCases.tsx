83796c4c8bc40f4f100fc76ac239c7ac6c29b0d1
Ugnius Saulevičius <ugnius.saulevicius@gmail.com>
2023-12-18 17:10:21 +0200

    bring back data fetching form endpoint

--- nvsc-lpl-app/src/hooks/UseCases.tsx
+++ nvsc-lpl-app/src/hooks/UseCases.tsx
@@ -1,28 +1,19 @@
 import { useEffect, useState } from 'react';
-//import axios from 'axios';
-// import { Cases, Case } from '../types/Cases';
-import { Case } from '../types/Cases';
-import localCases from '../data.json'; 
-
+import axios from 'axios';
+import { Cases, Case } from '../types/Cases';
 
 export const useCases = () => {
   const [cases, setCases] = useState<Case[]>();
   const [error, setError] = useState('');
 
   useEffect(() => {
-    //endpoint implementation
-    // axios
-    //   .get<Cases>('https://legioneles-atvejai.dekani9422.workers.dev/')
-    //   .then((res) => setCases(res.data['_data']))
-    //   .catch(err => {
-    //     setError(err.message);
-    //   });
-    try {
-      // Assuming your JSON structure is similar to what you were fetching from the API
-      setCases((localCases['_data'] as Case[]));
-    } catch (err) {
-      setError('Error loading cases');
-    }
+    axios
+      .get<Cases>('https://legioneles-atvejai.dekani9422.workers.dev/')
+      .then((res) => setCases(res.data['_data']))
+      .catch(err => {
+        setError(err.message);
+      });
+
 
   }, []);
 
